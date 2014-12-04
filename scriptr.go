ed20c7862ff082ebb6a75361f5c003b24fdadfe7
Justin Garrison <justinleegarrison@gmail.com>
2014-12-04 08:23:22 -0800

    working version with arguments

--- scriptr.go
+++ scriptr.go
@@ -3,21 +3,18 @@ package main
 import "os"
 import "syscall"
 import "os/exec"
-import "fmt"
 
 func main() {
 
 	args := []string{os.Args[1]}
 
-	fmt.Println(args)
 	binary, lookErr := exec.LookPath(os.Args[1])
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
-	if os.Args[2] != "" {
-		args = append(args, os.Args[2])
-		fmt.Println(args)
+	for i := 3; i <= len(os.Args); i++ {
+		args = append(args, os.Args[i-1])
 	}
 
 	env := os.Environ()
