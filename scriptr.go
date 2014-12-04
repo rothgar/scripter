53586202025e3c4df126b63d2ebdc6b8e18dccd4
Justin Garrison <justinleegarrison@gmail.com>
2014-12-04 09:46:29 -0800

    added comments

--- scriptr.go
+++ scriptr.go
@@ -6,19 +6,24 @@ import "os/exec"
 
 func main() {
 
+	// set executable passed as first arg
 	args := []string{os.Args[1]}
 
+	// look for the executeble in the path and reletive to scriptr path
 	binary, lookErr := exec.LookPath(os.Args[1])
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
+	// add args if more were passed
 	for i := 3; i <= len(os.Args); i++ {
 		args = append(args, os.Args[i-1])
 	}
 
+	// load current environment
 	env := os.Environ()
 
+	// execute and look for errors
 	execErr := syscall.Exec(binary, args, env)
 	if execErr != nil {
 		panic(execErr)
